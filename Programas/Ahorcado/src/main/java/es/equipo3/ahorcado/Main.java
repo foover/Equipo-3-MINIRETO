7d14c8c6e4b87e087f0e28e67a6ded7d90b26211
Daniel Zabala <dzabalav01@educantabria.es>
2025-12-17 19:47:25 +0100

    Versión 0.1 del ahorcado
    Se ha avanzado en el desarrollo del juego del ahorcado.

---
+++ Programas/Ahorcado/src/main/java/es/equipo3/ahorcado/Main.java
@@ -0,0 +1,11 @@
+package es.equipo3.ahorcado;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Ahorcado ahorcado = new Ahorcado();
+        ahorcado.jugarAhorcado();
+    }
+}
+
